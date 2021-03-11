8c4b7574e124107942cb41bcf886990634552e63
Narista Neri Vadila <nerivadilanarista@gmail.com>
2021-03-11 15:02:53 +0700

    Tugas 2

---
+++ exercise2.1.py
@@ -0,0 +1,20 @@
+print("Exercise 2.1")
+
+#tipe data Boolean
+print("True")
+
+#tipe data String
+print("Ayo belajar Python")
+print('Belajar Python Sangat Mudah')
+
+#tipe data Integer
+print(25)
+
+#tipe data Float
+print(8.5)
+
+#tipe data Hexadecimal
+print("9a")
+
+#tipe data Complex
+print(3j)
